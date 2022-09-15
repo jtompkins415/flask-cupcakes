969046ab5d501a9aded11f6eaf37e36674cae534
jtompkins415 <john.tompkins415@gmail.com>
2022-09-15 08:33:44 -0700

    Connect Flask app to DB

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, session, flash
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcake_db'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'seCreETCUpcake234'
+
+connect_db(app)
+
